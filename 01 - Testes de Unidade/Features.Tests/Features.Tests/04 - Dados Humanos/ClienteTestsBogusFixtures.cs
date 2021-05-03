b5dd73ed2582982d6092a6c5f715c6a305fa1a66
A.Fernando <aristotelesfernando.jr@gmail.com>
2021-05-03 19:15:43 -0300

    Mock de testes

--- 01 - Testes de Unidade/Features.Tests/Features.Tests/04 - Dados Humanos/ClienteTestsBogusFixtures.cs
+++ 01 - Testes de Unidade/Features.Tests/Features.Tests/04 - Dados Humanos/ClienteTestsBogusFixtures.cs
@@ -2,6 +2,8 @@
 using Bogus.DataSets;
 using Features.Clientes;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit;
 
 namespace Features.Tests
@@ -16,27 +18,60 @@ namespace Features.Tests
     public class ClienteTestsBogusFixtures : IDisposable
     {
 
+        //public Cliente GerarClienteValido()
+        //{
+        //    var genero = new Faker().PickRandom<Name.Gender>();
+
+        //    // Exemplos
+        //    //var email = new Faker().Internet.Email("fernando", "junior");
+        //    //var clienteFaker = new Faker<Cliente>();
+        //    //clienteFaker.RuleFor(c => c.Nome, (f,c) => f.Name.FirstName());
+
+        //    var cliente = new Faker<Cliente>("pt_BR")
+        //        .CustomInstantiator(f => new Cliente(
+        //            Guid.NewGuid(),
+        //            f.Name.FirstName(genero),
+        //            f.Name.LastName(genero),
+        //            f.Date.Past(80, DateTime.Now.AddYears(-18)),
+        //            "",
+        //            true,
+        //            DateTime.Now))
+        //        .RuleFor(c => c.Email, (f, c) => f.Internet.Email(c.Nome.ToLower(), c.Sobrenome.ToLower()));
+
+        //    return cliente;
+        //}
+
         public Cliente GerarClienteValido()
+        {
+            return GerarClientes(1, true).FirstOrDefault();
+        }
+
+        public IEnumerable<Cliente> ObterClientesVariados()
+        {
+            var clientes = new List<Cliente>();
+
+            clientes.AddRange(GerarClientes(50, true).ToList());
+            clientes.AddRange(GerarClientes(50, false).ToList());
+
+            return clientes;
+        }
+
+        public IEnumerable<Cliente> GerarClientes(int quantidade, bool ativo)
         {
             var genero = new Faker().PickRandom<Name.Gender>();
 
-            // Exemplos
-            //var email = new Faker().Internet.Email("fernando", "junior");
-            //var clienteFaker = new Faker<Cliente>();
-            //clienteFaker.RuleFor(c => c.Nome, (f,c) => f.Name.FirstName());
-
-            var cliente = new Faker<Cliente>("pt_BR")
+            var clientes = new Faker<Cliente>("pt_BR")
                 .CustomInstantiator(f => new Cliente(
                     Guid.NewGuid(),
                     f.Name.FirstName(genero),
                     f.Name.LastName(genero),
                     f.Date.Past(80, DateTime.Now.AddYears(-18)),
                     "",
-                    true,
+                    ativo,
                     DateTime.Now))
                 .RuleFor(c => c.Email, (f, c) => f.Internet.Email(c.Nome.ToLower(), c.Sobrenome.ToLower()));
 
-            return cliente;
+            return clientes.Generate(quantidade);
         }
 
         public Cliente GerarClienteInvalido()
